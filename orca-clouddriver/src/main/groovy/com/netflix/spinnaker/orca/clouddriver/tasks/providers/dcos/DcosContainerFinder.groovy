008037a307b3999391901d53c28b820553c2f342
Michael Tweten <Michael.Tweten@cerner.com>
2017-01-18 15:09:10 -0600

    Uncomment DcosContainerFinder impl.

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/dcos/DcosContainerFinder.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/dcos/DcosContainerFinder.groovy
@@ -16,48 +16,48 @@
 
 package com.netflix.spinnaker.orca.clouddriver.tasks.providers.dcos
 
+import com.netflix.spinnaker.orca.pipeline.model.Pipeline
 import com.netflix.spinnaker.orca.pipeline.model.Stage
 
 class DcosContainerFinder {
   static void populateFromStage(Map operation, Stage stage) {
-    //WAT DO?
-//    // If this is a stage in a pipeline, look in the context for the baked image.
-//    def deploymentDetails = (stage.context.deploymentDetails ?: []) as List<Map>
-//
-//    def containers = (List<Map<String, Object>>) operation.containers
-//
-//    if (!containers) {
-//      containers = [operation.container]
-//    }
-//
-//    containers.forEach { container ->
-//      if (container.imageDescription.fromContext) {
-//        def image = deploymentDetails.find {
-//          // stageId is used here to match the source of the image to the find image stage specified by the user.
-//          // Previously, this was done by matching the pattern used to the pattern selected in the deploy stage, but
-//          // if the deploy stage's selected pattern wasn't updated before submitting the stage, this step here could fail.
-//          it.refId == container.imageDescription.stageId
-//        }
-//        if (!image) {
-//          throw new IllegalStateException("No image found in context for pattern $container.imageDescription.pattern.")
-//        } else {
-//          container.imageDescription = [registry: image.registry, tag: image.tag, repository: image.repository]
-//        }
-//      }
-//
-//      if (container.imageDescription.fromTrigger) {
-//        if (stage.execution instanceof Pipeline) {
-//          Map trigger = ((Pipeline) stage.execution).trigger
-//
-//          if (trigger?.account == container.imageDescription.account && trigger?.repository == container.imageDescription.repository) {
-//            container.imageDescription.tag = trigger.tag
-//          }
-//        }
-//
-//        if (!container.imageDescription.tag) {
-//          throw new IllegalStateException("No tag found for image ${container.imageDescription.registry}/${container.imageDescription.repository} in trigger context.")
-//        }
-//      }
-//    }
+    // If this is a stage in a pipeline, look in the context for the baked image.
+    def deploymentDetails = (stage.context.deploymentDetails ?: []) as List<Map>
+
+    def containers = (List<Map<String, Object>>) operation.containers
+
+    if (!containers) {
+      containers = [operation.container]
+    }
+
+    containers.forEach { container ->
+      if (container.imageDescription.fromContext) {
+        def image = deploymentDetails.find {
+          // stageId is used here to match the source of the image to the find image stage specified by the user.
+          // Previously, this was done by matching the pattern used to the pattern selected in the deploy stage, but
+          // if the deploy stage's selected pattern wasn't updated before submitting the stage, this step here could fail.
+          it.refId == container.imageDescription.stageId
+        }
+        if (!image) {
+          throw new IllegalStateException("No image found in context for pattern $container.imageDescription.pattern.")
+        } else {
+          container.imageDescription = [registry: image.registry, tag: image.tag, repository: image.repository]
+        }
+      }
+
+      if (container.imageDescription.fromTrigger) {
+        if (stage.execution instanceof Pipeline) {
+          Map trigger = ((Pipeline) stage.execution).trigger
+
+          if (trigger?.account == container.imageDescription.account && trigger?.repository == container.imageDescription.repository) {
+            container.imageDescription.tag = trigger.tag
+          }
+        }
+
+        if (!container.imageDescription.tag) {
+          throw new IllegalStateException("No tag found for image ${container.imageDescription.registry}/${container.imageDescription.repository} in trigger context.")
+        }
+      }
+    }
   }
 }
